e9e0c7faadad53fac5b7d0ca6460d32694c38d55
HieuViper <mr.hieu2491@gmail.com>
2022-08-18 23:19:08 +0700

    update homepage

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useHistory } from 'react-router-dom'
+import wallpaper from '../animation/wallpaper2.jpg'
 
 const HomePage = () => {
   const history = useHistory()
@@ -14,9 +15,9 @@ const HomePage = () => {
   }, [history])
 
   return (
-    <div className='h-[100vh] relative flex items-center flex-col'>
-      <img src="./wallpaper2.jpg" alt="" className=''/>
-      <div className="absolute top-52">
+    <div className='h-[100vh] relative flex items-center flex-col bg-wallpaper bg-cover bg-center justify-center' >
+      {/* <img src="./wallpaper2.jpg" alt="" className=''/>  */}
+      <div className="">
         <span className='uppercase text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#000000] via-[#52c234] to-[#000000] animate-text'>Green Chat</span>
         <div className="flex flex-col gap-3 mt-7 text-center">
           <Link to="/sign-up" className="shadow-[0_6px_0_rgb(0,0,0)] hover:shadow-[0_3px_0px_rgb(0,0,0)] text-black ease-out hover:translate-y-1 transition-all py-3 px-9 bg-white rounded-3xl font-semibold text-lg">Sign Up</Link>
